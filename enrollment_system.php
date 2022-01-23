e21af386b5c3bcee07fedd6aed502167b132f37c
rayven2129 <rayven620@gmail.com>
2022-01-23 20:20:14 +0800

    Yehey nagawa ko na

--- enrollment_system.php
+++ enrollment_system.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Enrollment_System</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Enrollment_System</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/f9a76d52b7.js" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="js/enrollment_form.js"></script>
+
 </head>
 <body>
 	<div class="container-fluid div-header">
@@ -30,6 +31,7 @@
 						<option value="Transferee">Transferee</option>
 					</select>
 					<input type="hidden" name="student_type" id="student_type_value">
+
 					</td>
 				</tr>
 				<tr>
